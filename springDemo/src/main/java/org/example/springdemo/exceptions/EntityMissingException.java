fa719c3a95583c8805dc874b8235d8f319cdcbc3
Miroslav Stoev <soboko@abv.bg>
2024-04-23 21:59:29 +0300

    Delete springDemo directory

--- springDemo/src/main/java/org/example/springdemo/exceptions/EntityMissingException.java
+++
@@ -1,7 +0,0 @@
-package org.example.springdemo.exceptions;
-
-public class EntityMissingException extends RuntimeException{
-    public EntityMissingException(String msg) {
-        super(msg);
-    }
-}
