39066e6edb9df0ac0fad50785bcc4bb29406629a
Miroslav Stoev <soboko@abv.bg>
2024-12-05 13:16:49 +0200

    Add files via upload

---
+++ problems/meet-a-stranger/meet-a-stranger/src/main/java/com/epam/rd/autotasks/meetastranger/A.java
@@ -0,0 +1,10 @@
+package com.epam.rd.autotasks.meetastranger;
+
+class PrintTest {
+    public static void main(String[] args) {
+
+        System.out.println("2 + 2 = " + 2 + 2);
+
+        System.out.println(12345 + 5432L);
+    }
+}
