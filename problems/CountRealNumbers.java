a0850647082e7d27c77c3bf8af26e3f301182d16
Miroslav Stoev <soboko@abv.bg>
2024-12-14 15:27:28 +0200

    Add files via upload

--- problems/CountRealNumbers.java
+++ problems/CountRealNumbers.java
@@ -6,37 +6,24 @@ public class CountRealNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        List<Double> numbers =Arrays.stream(scanner.nextLine().split("\\s+")).map(Double::parseDouble).collect(Collectors.toList());
+        List<Double> numbers = Arrays.stream(scanner.nextLine().split("\\s+")).map(Double::parseDouble).toList();
 
-        Map<Double, Integer> mapCounter = new TreeMap<>();
+        Map<Double, Integer> mapCounter = new LinkedHashMap<>();
 
         for (double num : numbers) {
-
             if (!mapCounter.containsKey(num)) {
                 mapCounter.put(num, 1);
-
                 //mapCounter.put(num, 0);
-
-
-            }else {
-
+            } else {
                 int occurrences = mapCounter.get(num);
                 occurrences++;
                 mapCounter.put(num, occurrences);
-
                 // mapCounter.put(num, mapCounter.get(num) + 1);
-
             }
-
-
-
         }
         for (Map.Entry<Double, Integer> entry : mapCounter.entrySet()) {
-            DecimalFormat df = new DecimalFormat("#.#######");
-            System.out.printf("%s -> %d%n", df.format(entry.getKey()), entry.getValue());
-
+          //  DecimalFormat df = new DecimalFormat("#.#######");
+            System.out.printf("%s -> %d%n", (entry.getKey()), entry.getValue());
         }
-
-
     }
 }
---
+++ problems/LargestNumbers.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+
+public class LargestNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Arrays.stream(scanner.nextLine().split("\\s+"))
+                .map(Integer::parseInt)
+                .sorted(
+                        Comparator.reverseOrder()   //(left, right) -> right.compareTo(left)
+                ).limit(3)
+                .forEach(num -> System.out.printf("%d ", num));
+
+    }
+}
